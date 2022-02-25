6660319cec6093de667286e2de4562934f25371d
Andrea655 <99601894+Andrea655@users.noreply.github.com>
2022-02-25 12:52:55 +0100

    Update fill.php

--- commande/fill.php
+++ commande/fill.php
@@ -6,4 +6,3 @@ $pdo = new PDO('mysql:dbname=blog2.0;host=127.0.0.1', 'root','',[
 $pdo->exec('TRUNCATE TABLE post_category');
 $pdo->exec('TRUNCATE TABLE post');
 $pdo->exec('TRUNCATE TABLE category');
-$pdo->exec('TRUNCATE TABLE user');
