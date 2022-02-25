ec5c152cba3a44611a81a4fd49b1e1ff705011af
Andrea655 <99601894+Andrea655@users.noreply.github.com>
2022-02-25 12:55:09 +0100

    Update data.php

---
+++ public/data.php
@@ -0,0 +1,22 @@
+
+<?php
+class db{
+    private $hote = 'localhost'; // le chemin vers le serveur
+    private $base = 'base'; // le nom de votre base de donnée
+    private $utilisateur = 'root'; // nom d'utilisateur pour se connecter
+    private $pass = ''; //mot de passe pour se connecter
+    private $db;
+ 
+    public function __construct($hote= null, $base=null, $utilisateur=null, $pass=null){
+        if($hote != null){
+            $this->hote = $hote;
+            $this->base = $base;
+            $this->utlisateur = $utilisateur;
+            $this->pass = $pass;
+        }
+        try{
+            $this->db = new PDO('mysql:host='.$this->hote.';dbname='.$this->base, $this->utilisateur, $this->pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8',
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
+                ));
+        }catch(PDOException $e){
+            die('<h1>Impossible de se connecter</h1>');
