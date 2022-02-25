3e0011cb08d45723f9e4fe2357fc2b190fdcc565
Andrea655 <99601894+Andrea655@users.noreply.github.com>
2022-02-25 13:15:56 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -10,3 +10,34 @@ $router
         ->get('/blog','/post/index', 'blog');
         ->get('/blog/category','/category/show','category');
          ->run();
+
+
+<html>
+      
+   require '../views/layouts/default.php';
+<body>
+  
+  
+  
+<form action="../test.php" method="post">
+    <div>
+        <label for="name">Nom utilisateur:</label>
+        <input type="text" id="name" name="pseudo">
+    </div>
+     <div>
+        <label for="name">Prenom utilisateur:</label>
+        <input type="text" id="name" name="pseudo">
+    </div>
+    <div>
+        <label for="mdp">Mot de passe :</label>
+        <input type = "password" id="mdp" name="mdp"></input>
+    </div>
+  
+    <div class="button">
+        <button type="submit">Inscription</button>
+    </div>
+      
+</body>
+</form>
+
+</html>
